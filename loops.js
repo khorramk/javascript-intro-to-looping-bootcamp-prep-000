8e2c41ba4eac027f2752dff4c8a450bd1ffa6099
Khorram Khondkar <khorramk.kbsk@gmail.com>
2018-01-16 19:15:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,13 @@
 function forLoop(array){
-  var b;
+  
   for (var i=0; i <= 25; i++){
-     return i === 1 ? "I am 1 strange loop.": `i am ${i} strange loops.`;
+     if (i === 1){
+       array.push("I am 1 strange loop.");
+       
+     }else{
+       array.push(`i am ${i} strange loops.`);
+     }
     
   }
-  
+  return array;
 }
